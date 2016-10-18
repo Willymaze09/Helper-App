a2dd7a9e67024268c4def79328d32a954a9e6209
Hayes Warner <hayes.warner@gmail.com>
2016-10-17 19:08:20 -0600

    Add files via upload

---
+++ User_Help.ps1
@@ -0,0 +1,5 @@
+﻿$input = Read-Host -prompt 'Enter the printer name'
+$printServer = "\\NVPADMPRT01\"
+
+#Adds the printer the user enters into the console window
+(New-Object -ComObject WScript.Network).AddWindowsPrinterConnection($printServer+$input)
